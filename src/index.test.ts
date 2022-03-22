b42d95fa246bc40bd680a37981ce12180b1a6ecc
Baba Jaga <85198775+d1f1@users.noreply.github.com>
2022-03-22 13:49:46 +0100

    Refactoring.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,20 +1,19 @@
 import { TurboNumber } from "./index";
 
 describe("TurboNumber()", () => {
+  let tn: TurboNumber;
+  beforeEach(() => {
+    tn = new TurboNumber(10);
+  });
   it("10 divide by 2 returns 5.", () => {
-    const tn = new TurboNumber(10)
-    tn.divide(2);
-    expect(tn.result()).toBe(5);
+    expect(tn.divide(2).result()).toBe(5);
   });
 
   it("10 subtract by 2 returns 8.", () => {
-    const tn = new TurboNumber(10)
-    tn.subtract(2);
-    expect(tn.result()).toBe(8);
+    expect(tn.subtract(2).result()).toBe(8);
   });
 
   it("Throw error when divide by 0.", () => {
-    const tn = new TurboNumber(10)
     expect(() => tn.subtract(0)).toThrow('Cannot divide by 0.');
   });
 
