1f109c78f3b369d040323e0396deb1abef103550
Baba Jaga <85198775+d1f1@users.noreply.github.com>
2022-03-22 13:31:37 +0100

    Simple division test added.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,9 @@
 import { number } from "./index";
 
-describe("number", function () {
-  it("works", () => {
-    expect(number).toBe(5);
+describe("TurboNumber()", () => {
+  it("10 divide by 2 returns 5.", () => {
+    const tn = new TurboNumber(10)
+    tn.divide(2);
+    expect(tn.result).toBe(5);
   });
 });
