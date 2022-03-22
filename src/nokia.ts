cdeae0849717a8fa72acb342b5a46b5eb308318a
Baba Jaga <85198775+d1f1@users.noreply.github.com>
2022-03-22 15:15:52 +0100

    ErrorHandling - MessageTooLong

--- src/nokia.ts
+++ src/nokia.ts
@@ -1,7 +1,8 @@
 export class NokiaTexter {
   constructor(private text: string) {}
   squeeze() {
-    const formatedText = this.text;
+    const formatedText = this.text.replace(/\s/g, "");
+    if (formatedText.length > 160) throw new Error("MessageTooLong");
     return formatedText;
   }
 }
