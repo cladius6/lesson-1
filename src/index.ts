c14e5273805dd34114ab9467fcb70018db600c9b
Baba Jaga <85198775+d1f1@users.noreply.github.com>
2022-03-22 10:20:50 +0100

    TurboNumber added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-export const number = 5;
+export class TurboNumber {
+  private x: number;
+  private result: number;
+  constructor(x: number) {
+    this.x = x;
+    this.result = 0;
+  }
+  substract(y: number): void{
+    this.result = this.x-y;
+  }
+  divide(y: number): void{
+    if (y == 0) throw 'Cannot divide by zero.';
+    this.result = this.x/y;
+  }
+  public getResult(): number {
+    return this.result;
+  }
+}
